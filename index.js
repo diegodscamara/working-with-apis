c8cc25e97ed2e58357d1f4163e927611e30ff51c
Diego Câmara <diego.camara@nsh.com.br>
2022-05-23 17:31:54 -0300

    use method, post with insomnia app

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express()
 
 app.listen('3000')
 
-app.route('/').get((req, res) => res.send('hello'))
-app.route('/sobre').get((req, res) => res.send('hello sobre'))
+//middleware 
+
+app.use(express.json())
+
+app.route('/').post((req, res) => res.send(req.body))
