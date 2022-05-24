7ca1970884dff269db6c08c51ace8d18a245d31a
Diego Câmara <diego.camara@nsh.com.br>
2022-05-24 10:47:40 -0300

    Route params with express

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.listen('3000')
 
-//middleware
-app.use(express.json())
-
-app.route('/').post((req, res) => {
-  const { nome, cidade, livros } = req.body
-  res.send(`Meu nome é ${nome} e minha cidade é ${cidade} e meus livros são ${livros}`)
-})
+app.route('/').get((req, res) => res.send('oi'))
+app.route('/:variavel').get((req, res) => res.send(req.params.variavel))
+app.route('/identidade/:nome').get((req, res) => res.send(req.params.nome))
