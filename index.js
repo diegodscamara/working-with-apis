ed5480c67f142389100159f490d8826b610d54bb
Diego Câmara <diego.camara@nsh.com.br>
2022-05-24 10:53:53 -0300

    Query params with express

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const app = express()
 
 app.listen('3000')
 
-app.route('/').get((req, res) => res.send('oi'))
-app.route('/:variavel').get((req, res) => res.send(req.params.variavel))
-app.route('/identidade/:nome').get((req, res) => res.send(req.params.nome))
+app.route('/').get((req, res) => res.send(req.query.nome))
+app.route('/about/user').get((req, res) => res.send(req.query.nome))
