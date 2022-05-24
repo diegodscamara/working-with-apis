c4260e25c902a7b120579dd582edf7892e2ec047
Diego Câmara <diego.camara@nsh.com.br>
2022-05-24 14:42:48 -0300

    POST method to create new users

--- fetch-discover-api/main.js
+++ fetch-discover-api/main.js
@@ -18,5 +18,25 @@ function getUser() {
     .catch(error => console.error(error))
 }
 
+function addUser(newUser) {
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(newUser),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+const newUser = {
+  name: 'Vanessa Camara',
+  avatar: 'http://picsum.photos/200/300',
+  city: 'Esperança'
+}
+addUser(newUser)
+
 getUsers()
 getUser()
